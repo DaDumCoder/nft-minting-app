7adda140a2b7bf9a39dc945a70bf2a218a3c5992
D-Dum <piyushankit1410@gmail.com>
2025-03-01 18:46:51 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, compose, createStore, combineReducers } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk"; // Line 2: Fixed named import for 3.1.0
 import blockchainReducer from "./blockchain/blockchainReducer";
 import dataReducer from "./data/dataReducer";
 
